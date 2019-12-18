e8cbca7286e4b427e0fdf42301b9b0d2fe968dd6
Julia Bouvier <juliabouv@gmail.com>
2019-12-18 10:07:21 -0800

    Add movie to library

--- src/components/Search.js
+++ src/components/Search.js
@@ -11,28 +11,71 @@ class Search extends Component {
     this.state = {
       selectedMovie: {},
       error: '',
+      isInRentalLibrary: false,
     }
   }
 
+  resetState = () => {
+    this.setState({
+      selectedMovie: {},
+    });
+  }
+
   searchMovie = (query) => {
-    console.log('querry is', query)
-    axios.get('http://localhost:4000/movies', {
-      params: {
-        query: query,
-      }
-    })
+    if (query !== null) {
+      axios.get('http://localhost:4000/movies', {
+        params: {
+          query: query,
+        }
+      })
+      .then((response) => {
+        console.log(response.data[0])
+        this.setState({
+          selectedMovie: response.data[0]
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          error: error.errors,
+          selectedMovie: {},
+        });
+      })
+    }
+    // this.localSearch()
+  }
+
+  addMovie = () => {
+    axios.post('http://localhost:4000/movies', this.state.selectedMovie)
     .then((response) => {
-      console.log(response.data[0])
-      this.setState({
-        selectedMovie: response.data[0]
-      });
+      console.log(response)
     })
     .catch((error) => {
+      console.log(error)
       this.setState({
-        error: error.errors
-      });
+        error: error.message,
+        selectedMovie: {},
+      })
     })
+    // this.resetState()
   }
+
+  // localSearch = () => {
+  //   axios.get(`http://localhost:4000/movies/${this.state.selectedMovie.title}`)
+  //   .then((response) => {
+  //     if (this.state.selectedMovie && response.data.title === this.state.selectedMovie.title) {
+  //       this.setState({
+  //         isInRentalLibrary: true,
+  //       })
+  //     }
+  //     console.log(this.state.isInRentalLibrary)
+  //   })
+  //   .catch((error) => {
+  //     this.setState({
+  //       isInRentalLibrary: false,
+  //       error: error.errors,
+  //     })
+  //   })
+  // }
 
   render() {
     return (
@@ -40,11 +83,12 @@ class Search extends Component {
         <SearchBar
           submitSearchTermCallback={this.searchMovie}
         />
-
-        {/* {console.log(this.state.selectedMovie)} */}
         {this.state.error}
-        {/* {this.state.selectedMovie} */}
-        {/* {this.state.selectedMovie.title} */}
+        {Object.values(this.state.selectedMovie).length !== 0 ? <img src={this.state.selectedMovie.image_url} alt={this.state.selectedMovie.title}></img> : null}
+        {/* {this.state.isInRentalLibrary && this.state.selectedMovie.title !== undefined ? 'Movie is in Rental Library' : 'Movie is Not in Rental Library'} */}
+        {console.log(this.state.selectedMovie)}
+        {Object.values(this.state.selectedMovie).length !== 0 ? <button onClick={this.addMovie}>Add Movie to Inventory</button> : null}
+        
       </section>
     );
   }
